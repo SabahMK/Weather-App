2a4cac38ebb509e1dca632c86ac79f9905bb52f1
SabahMK <sabahmk03@gmail.com>
2020-01-27 17:51:55 +0530

    first commit

---
+++ scripts/forcast.js
@@ -0,0 +1,29 @@
+const key = 'eLdsqHGObJ5zVjcNOkzbK6Jwj1sR9F3V'
+
+//Get weather informations:
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0]
+}
+
+
+//Get city informations:
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0]
+}
+
+// getCity('bangalore')
+//     .then(data => getWeather(data.Key))  //Get promise from getweather
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+
+// getWeather('349727');
